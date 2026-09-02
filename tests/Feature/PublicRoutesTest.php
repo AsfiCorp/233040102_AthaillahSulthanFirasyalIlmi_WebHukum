3f38d8b8a7b147a6f39b38742f67762f508e7b08
AsfiCorp <athaillahsfi@gmail.com>
2026-09-02 18:00:40 +0700

    test: add feature tests for public routes

---
+++ tests/Feature/PublicRoutesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+test('home page returns a successful response', function () {
+    $response = $this->get('/');
+    $response->assertStatus(200);
+});
+
+test('advocates index page returns a successful response', function () {
+    $response = $this->get('/advocates');
+    $response->assertStatus(200);
+});
+
+test('news index page returns a successful response', function () {
+    $response = $this->get('/news');
+    $response->assertStatus(200);
+});
+
+test('contact page returns a successful response', function () {
+    $response = $this->get('/contact');
+    $response->assertStatus(200);
+});
